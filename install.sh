457b347d32dac02ab7b5261d711caee66e1bf91c
MiHaTsKiYi13 <trotskyi.mikhail@gmail.com>
2025-08-02 14:42:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,40 @@
+#!/bin/bash
+
+
+source /etc/os-release
+
+echo "[+] Installing build tools..."
+
+case "$ID" in
+    arch | manjaro)
+        sudo pacman -Sy --noconfirm base-devel
+        ;;
+    ubuntu | debian)
+        sudo apt update && sudo apt install -y build-essential
+        ;;
+    fedora)
+        sudo dnf install -y make automake gcc gcc-c++ kernel-devel
+        ;;
+    opensuse* | sles)
+        sudo zypper install -y make gcc
+        ;;
+    alpine)
+        sudo apk add make gcc musl-dev
+        ;;
+    *)
+        echo "Unsupported distro: $ID"
+        exit 1
+        ;;
+esac
+
+echo "[+] Building mfetch..."
+
+make
 sudo make install
+
 chmod +x mfetch
+
 ./mfetch
-echo mfetch is now installed!
+
+# Уведомление
+echo "mfetch is now installed!"
